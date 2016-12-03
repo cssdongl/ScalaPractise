d623e01e3741f6e5cd8a5f6b2b8c1c5203cf3861
SynLz <cssdongl@163.com>
2016-12-03 17:44:41 +0800

    spark streaming test

---
+++ src/main/scala/org/ldong/spark/sparkstreaming/redis/RedisClient.scala
@@ -0,0 +1,25 @@
+package org.ldong.spark.sparkstreaming.redis
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig
+import redis.clients.jedis.JedisPool
+
+/**
+  * @author cssdongl@gmail.com
+  * @date 2016/12/3 16:19  
+  * @version V1.0
+  */
+object RedisClient {
+  val redisHost = "10.10.4.130"
+  val redisPort = 6379
+  val redisTimeout = 30000
+  lazy val pool = new JedisPool(new GenericObjectPoolConfig(), redisHost, redisPort, redisTimeout)
+
+  lazy val hook = new Thread {
+    override def run = {
+      println("Execute hook thread: " + this)
+      pool.destroy()
+    }
+  }
+  sys.addShutdownHook(hook.run)
+
+}
