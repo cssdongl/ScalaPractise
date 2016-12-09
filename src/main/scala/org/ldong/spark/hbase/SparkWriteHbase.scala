67b601bf294c9dfd3e04267455305b7ac90ee59b
SynLz <cssdongl@163.com>
2016-12-09 17:47:30 +0800

    spark write hbase test

---
+++ src/main/scala/org/ldong/spark/hbase/SparkWriteHbase.scala
@@ -0,0 +1,58 @@
+package org.ldong.spark.hbase
+
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapred.TableOutputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{HBaseConfiguration, HConstants}
+import org.apache.hadoop.mapred.JobConf
+import org.apache.spark.{SparkConf, SparkContext}
+import org.ldong.spark.common.PropertiesUtil
+import org.apache.hadoop.hbase.HBaseConfiguration
+
+import scala.collection.JavaConversions
+
+/**
+  * @author cssdongl@gmail.com
+  * @date 2016/12/9 18:07
+  * @version V1.0
+  */
+object SparkWriteHbase extends App{
+  val conf = new SparkConf().setAppName("test").setMaster("local[2]")
+  val sc = new SparkContext(conf)
+
+  if(args.length < 1){
+    sys.error("Need input file path")
+    sys.exit(1)
+  }
+
+  /**
+  0015A49A8F2A60DACEE0160545C58F94,1234
+  0152C9666B5F3426DDDB5FB74BDBCE4F,4366
+  0160D90AC268AEB595208E8448C7F8B8,6577
+  0225A39EB29BDB582CA58BE86791ACBC,1234
+  02462ACDF7232C49890B07D63B50C5E1,4366
+    */
+
+  val data = sc.textFile(args(0))
+
+  data.foreach(println)
+
+  val hConf = HBaseConfiguration.create()
+  val zookeeper = PropertiesUtil.getValue("ZOOKEEPER_ADDRESS")
+  hConf.set(HConstants.ZOOKEEPER_QUORUM,zookeeper)
+
+  val jobConf = new JobConf(hConf,this.getClass)
+  jobConf.set(TableOutputFormat.OUTPUT_TABLE,"spark_hbase")
+  jobConf.setOutputFormat(classOf[TableOutputFormat])
+
+  val result = data.map { item =>
+    val Array(key,value) = item.split(",")
+    val rowKey = key.reverse
+    val put = new Put(Bytes.toBytes(rowKey))
+    put.addColumn(Bytes.toBytes("info"),Bytes.toBytes("column1"),Bytes.toBytes(value))
+    (new ImmutableBytesWritable(),put)
+  }
+  result.saveAsHadoopDataset(jobConf)
+  sc.stop()
+}
--- src/main/scala/org/ldong/spark/mr/CalculateJoin.scala
+++ src/main/scala/org/ldong/spark/mr/CalculateJoin.scala
@@ -1,6 +1,7 @@
 package org.ldong.spark.mr
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -8,21 +9,37 @@ import org.apache.spark.{SparkConf, SparkContext}
   * @date 2016/12/9 11:53  
   * @version V1.0
   */
-object CalculateJoin extends App{
+object CalculateJoin extends App {
   val conf = new SparkConf().setAppName("test join").setMaster("local[1]")
   val sc = new SparkContext(conf)
 
   val sqlContext = new SQLContext(sc)
 
-  val idName = sc.parallelize(Array((1,"zhangsan"),(2,"lisi"),(3,"wangwu")))
+  val idName = sc.parallelize(Array((1, "zhangsan"), (2, "lisi"), (3, "wangwu")))
 
-  val idAge = sc.parallelize(Array((1,20),(2,25),(4,30)))
+  val idAge = sc.parallelize(Array((1, 20), (2, 25), (4, 30)))
 
   idName.join(idAge).collect().foreach(println)
 
   idName.rightOuterJoin(idAge).collect().foreach(println)
 
   idName.leftOuterJoin(idAge).collect().foreach(println)
+
+  val schema1 = StructType(Array(StructField("id", DataTypes.IntegerType, nullable = true),
+    StructField("name", DataTypes.StringType, nullable = true)))
+  val idNameDF = sqlContext.createDataFrame(idName.map(t => Row(t._1, t._2)), schema1)
+
+  val schema2 = StructType(Array(StructField("id", DataTypes.IntegerType, nullable = true),
+    StructField("age", DataTypes.IntegerType, nullable = true)))
+  val idAgeDF = sqlContext.createDataFrame(idAge.map(t => Row(t._1, t._2)), schema2)
+
+  idNameDF.join(idAgeDF, idNameDF("id") === idAgeDF("id"), "left_outer").collect().foreach(println)
+
+  idNameDF.groupBy("id").count().show()
+
+  idAgeDF.filter(idAgeDF("age") > 23).show()
+
+  idAgeDF.select("age").show()
 
   sc.stop()
 }
