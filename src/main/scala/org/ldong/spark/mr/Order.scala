1ab50a6f84753f283f5793f2f5696edd9fa736e4
dongliang <cssdongl@163.com>
2016-12-10 21:11:50 +0800

    backup some test

--- src/main/scala/org/ldong/spark/mr/Order.scala
+++ src/main/scala/org/ldong/spark/mr/Order.scala
@@ -10,7 +10,16 @@ object Order extends App {
   val sc = new SparkContext(conf)
   val data = List(1, 4, 2, 9, 3)
   val rdd = sc.parallelize(data)
+  //by default the sort is ascend order
   val sortResult = rdd.sortBy(x => x).collect
   sortResult.foreach(println)
+
+  val sortResult1 = rdd.sortBy(x => x,false).collect
+  sortResult1.foreach(println)
+
+  //can change the partition size while sort
+  val sortResult2 = rdd.sortBy(x => x,false,1).collect
+  sortResult2.foreach(println)
+
   sc.stop()
 }
