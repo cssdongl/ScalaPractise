731ad0891c144979206e00b04592d3a7bcf34b0b
SynLz <cssdongl@163.com>
2017-04-26 15:48:44 +0800

    spark test many small files

---
+++ src/main/scala/org/ldong/spark/sql/SparkMergeMultiSmallFiles.scala
@@ -0,0 +1,43 @@
+package org.ldong.spark.sql
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @author cssdongl@gmail.com
+  * @date 2016/12/17 11:35  
+  * @version V1.0
+  */
+object SparkMergeMultiSmallFiles extends App {
+  val conf = new SparkConf().setAppName("test small files performance").setMaster("yarn-cluster")
+  val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+
+  val data2 = sc.wholeTextFiles("/jjbox/open/2017/*/*/*/*txt",5)
+
+  val valuesRdd = data2.values
+
+  val jsons = sqlContext.jsonRDD(valuesRdd)
+
+  jsons.registerTempTable("device_open_normal")
+
+  val joinsResult = sqlContext.sql("SELECT DISTINCT a.devicewifimac, a.deviceusid FROM (SELECT DISTINCT don.devicewifimac, don.deviceusid, substr(don.opentime, 1, 10) AS time1 FROM device_open_normal don WHERE don.deviceusid NOT IN ('0123456789abcdef', '0123456789ABCDEF') ) a JOIN (SELECT DISTINCT don.devicewifimac, don.deviceusid, substr(don.opentime, 1, 10) AS time2 FROM device_open_normal don WHERE don.deviceusid = '0123456789abcdef' OR don.deviceusid = '0123456789ABCDEF' ) b ON a.devicewifimac = b.devicewifimac WHERE a.time1 < b.time2")
+  joinsResult.toJavaRDD.coalesce(1,true).saveAsTextFile("/test/dongliang/sparkResults")
+
+//  val lines = sc.newAPIHadoopFile("/jjbox/open/2017/*/*/*/*",
+//    classOf[CombineTextInputFormat], classOf[LongWritable], classOf[Text])
+//
+//  val jsonObj = lines.values.map {jsonLine =>
+//    val data = JSONObject.fromObject(jsonLine)
+//    Some(data)
+//  }
+//
+//  val jsons = sc.parallelize(Seq(lines.map(_._2)))
+//
+//  val tableData = sqlContext.jsonRDD(jsons)
+
+
+  //  lines.values().filter { case (k, v) => v != null }.collect().foreach(println)
+
+  sc.stop()
+}
